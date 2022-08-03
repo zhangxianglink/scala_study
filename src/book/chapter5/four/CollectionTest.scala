225f7155e34eae219c4634c79b92f14b624505c8
zhangxiang <xiangzhang@x2era.com>
2022-08-03 13:48:09 +0800

    5.4 协变，逆变

--- src/book/chapter5/four/CollectionTest.scala
+++ src/book/chapter5/four/CollectionTest.scala
@@ -1,11 +1,15 @@
 package book.chapter5.four
 
 /**
- * 协变：接收一个基类实例的集合的地方，使用一个子类实例的集合（covariance）
- * 逆变：以上（contravariance）
- * 默认情况下scala都不支持
+ * 泛型方法
+ *  *  covariant 协变。使你能够使用比原始指定的类型的子类
+    Contravariance 逆变。使你能够使用比原始指定的类型的父类。
+    Invariance 不变。你只能使用原始指定的类型，不能协变和逆变
+    Upper bounds 上界。
+    Lower bounds 下界。
  */
 object CollectionTest {
+
 
   def copyPet[S , P >: S](son: Array[S], parent: Array[P]): Unit = {
     for (p <- parent){
@@ -25,13 +29,41 @@ object CollectionTest {
     }
   }
 
+  def usePet2[T <: Pet](pets: Array[T]): Unit = {
+    println(s"support covariance: ")
+    for (e <- pets){
+      println(e)
+    }
+  }
+
+  def usePet3[T >: Cat](pets: Array[T]): Unit = {
+    println("逆变")
+    for (e <- pets){
+      println(e)
+    }
+  }
+
+  def usePet4[T >: Randall <: Cat](pet: Array[T]): Unit = {
+    println(s"上下界: $pet")
+  }
+
   def main(args: Array[String]): Unit = {
     val cats = Array(new Cat("mark"), new Cat("lucy"))
 //      协变失败  new Cat("a") usePet(cats)
-//    new Cat("b") usePet2(cats)
+    // 泛型上界，协变
+    usePet2(cats)
+    // 泛型下界，逆变
+    usePet3(Array(new Pet("this is a pet")))
+    // 逆变失败 usePet3(Array(new WhiteRandall("this is a pet")))
+
+    // 上下界同时存在
+    usePet4(Array(new WhiteRandall("边界外布偶猫")))
+    usePet4(Array(new Randall("布偶猫")))
+    usePet4(Array(new Pet("动物")))
+    usePet4(Array(new Cat("猫")))
+
 
     val pets = Array(new Pet("a"), new Pet("b"))
-
     copyPet(cats, pets)
   }
 
@@ -45,11 +77,10 @@ class Cat(override val name: String) extends Pet(name: String){
   def usePet(pets: Array[Pet]): Unit ={
     println(s"${pets apply(1)}")
   }
+}
 
-  def usePet2[T <: Pet](pets: Array[T]): Unit = {
-    println(s"support covariance: ")
-    for (e <- pets){
-      println(e)
-    }
-  }
+class Randall(override val name: String) extends Cat(name: String){
+}
+
+class WhiteRandall(override val name: String) extends Randall(name: String){
 }
---
+++ src/book/chapter5/four/CollectionTest2.scala
@@ -0,0 +1,34 @@
+package book.chapter5.four
+
+/**
+ * * 泛型类
+ *  covariant 协变。使你能够使用比原始指定的类型的子类
+    Contravariance 逆变。使你能够使用比原始指定的类型的父类。
+    Invariance 不变。你只能使用原始指定的类型，不能协变和逆变
+    Upper bounds 上界。
+    Lower bounds 下界。
+ */
+object CollectionTest2 {
+
+    def main(args: Array[String]): Unit = {
+        val A = new tmp1[Parent](new Parent)
+//        val B: tmp1[Son] = A
+
+        val C = new tmp3[Parent](new Parent)
+        val D:tmp3[Son] = C
+
+        val E = new tmp2[Son](new Parent)
+        val F:tmp2[Parent] = E
+    }
+
+
+}
+
+class Parent
+class Son extends Parent
+
+class tmp1[T](t: T)
+// 协变类
+class tmp2[+T](t: T)
+// 逆变类
+class tmp3[-T](t: T)
